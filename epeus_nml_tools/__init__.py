9fc14a6f590d27b35ffd0103b6c27da82e3a9839
Jason Lantz <jasontlantz@gmail.com>
2020-07-31 19:37:40 -0500

    move SessionCookieDataFactory out of main

--- epeus_nml_tools/__init__.py
+++ epeus_nml_tools/__init__.py
@@ -2,7 +2,7 @@ import os
 from pyramid.config import Configurator
 from pyramid.session import SignedCookieSessionFactory
 
-
+session_factory = SignedCookieSessionFactory(os.environ.get("COOKIE_SECRET"))
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -11,6 +11,5 @@ def main(global_config, **settings):
         config.include("pyramid_jinja2")
         config.include(".routes")
         config.scan()
-        session_factory = SignedCookieSessionFactory(os.environ.get("COOKIE_SECRET"))
         config.set_session_factory(session_factory)
     return config.make_wsgi_app()
