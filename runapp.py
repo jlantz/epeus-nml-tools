73dd3ce69d09d32e16b474c139b9afc205f859fa
Jason Lantz <jasontlantz@gmail.com>
2020-08-01 19:56:53 -0500

    attempt to fix http/https handling on heroku

--- runapp.py
+++ runapp.py
@@ -5,6 +5,5 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app = loadapp('config:production.ini', relative_to='.')
-
-    serve(app, host='0.0.0.0', port=port)
+    app = loadapp("config:production.ini", relative_to=".")
+    serve(app, host="0.0.0.0", port=port)
